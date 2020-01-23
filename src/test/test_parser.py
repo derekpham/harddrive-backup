6fe19bc0d29506d728a99006fbbfe4ae51b4c98d
Derek Pham <anhdpham97@gmail.com>
2020-01-22 22:23:08 -0800

    Enforce that home dir and dest must be absolute

--- src/test/test_parser.py
+++ src/test/test_parser.py
@@ -1,11 +1,38 @@
 import unittest
 
 from parser import Parser
+from parser import InvalidConfigError
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_invalid_config(self):
+    def test_invalid_home_dir(self):
+        test_str = """
+        external-hard-drive-dest: "/run/media/foo/bar"
+        home-dir: "home/foo"
+
+        backup:
+        - "Documents": "." # /home/foo/Documents -> /run/media/foo/bar/Documents
+        - "/home/foo/Pictures": "/run/media/foo/bar" # /home/foo/Pictures -> /run/media/foo/bar/Pictures
+        - "Desktop": "/run/media/foo/bar" # /home/foo/Desktop -> /run/media/foo/bar/Desktop
+        - "/home/foo/Downloads": "Downloads" # /home/foo/Downloads -> /run/media/foo/bar/Downloads/Downloads
+        """
+        self.assertRaises(InvalidConfigError, Parser.str_to_backup_context, test_str)
+
+    def test_invalid_dest(self):
+        test_str = """
+        external-hard-drive-dest: "run/media/foo/bar"
+        home-dir: "/home/foo"
+
+        backup:
+        - "Documents": "." # /home/foo/Documents -> /run/media/foo/bar/Documents
+        - "/home/foo/Pictures": "/run/media/foo/bar" # /home/foo/Pictures -> /run/media/foo/bar/Pictures
+        - "Desktop": "/run/media/foo/bar" # /home/foo/Desktop -> /run/media/foo/bar/Desktop
+        - "/home/foo/Downloads": "Downloads" # /home/foo/Downloads -> /run/media/foo/bar/Downloads/Downloads
+        """
+        self.assertRaises(InvalidConfigError, Parser.str_to_backup_context, test_str)
+
+    def test_valid_config(self):
         test_str = """
         external-hard-drive-dest: "/run/media/foo/bar"
         home-dir: "/home/foo"
