a76103bfc4848e160f36c191cdd0101b239318ab
Derek Pham <anhdpham97@gmail.com>
2020-01-26 19:45:43 -0800

    use argparse for arguments parsing

--- src/main/main.py
+++ src/main/main.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python
 
 import sys
+import argparse
 
 from config_parser import ConfigParser
 from backup_service import RsyncBackupService
 
 
+def get_args():
+    arg_parser = argparse.ArgumentParser(description='Backs up files and folders using RSync '
+                                                     'and potentially some cloud storage '
+                                                     '(like S3 Glacier?)')
+    arg_parser.add_argument('backup_config_file', metavar='C', type=str,
+                            help='name of your backup config')
+    arg_parser.add_argument('--dry-run', dest='dry_run', action='store', default=False,
+                            help='if passed will just print out the input and output paths')
+    return arg_parser.parse_args()
+
+
 def main():
-    config_path = sys.argv[1]
-    raw_config = open(config_path).read()
+    args = get_args()
+    print(args)
+    """raw_config = open(config_path).read()
     backup_config = ConfigParser.str_to_backup_context(raw_config)
     print(backup_config)
 
-    RsyncBackupService(backup_config).run()
+    RsyncBackupService(backup_config).run()"""
 
 
 if __name__ == '__main__':
