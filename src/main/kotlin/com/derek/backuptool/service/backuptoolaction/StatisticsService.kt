567b9bb9ed9248c0afb317a85937ad7e24bc0f4e
Derek Pham <anhdpham97@gmail.com>
2020-04-13 23:40:01 -0400

    style

--- src/main/kotlin/com/derek/backuptool/service/backuptoolaction/StatisticsService.kt
+++ src/main/kotlin/com/derek/backuptool/service/backuptoolaction/StatisticsService.kt
@@ -2,6 +2,4 @@ package com.derek.backuptool.service.backuptoolaction
 
 import javax.inject.Inject
 
-class StatisticsService @Inject constructor() : BackupToolAction {
-
-}
+class StatisticsService @Inject constructor() : BackupToolAction
